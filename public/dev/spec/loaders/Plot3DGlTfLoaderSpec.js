acf00ae7f753390fe3ccef7a22f08d48d775d817
etk66296 <d_heile@web.de>
2021-06-08 07:37:47 +0200

    extracting data from gltf

--- public/dev/spec/loaders/Plot3DGlTfLoaderSpec.js
+++ public/dev/spec/loaders/Plot3DGlTfLoaderSpec.js
@@ -9,8 +9,12 @@ describe("Plot3DGlTfLoader", function() {
     expect(myPlot3DGlTfLoader.__proto__.__proto__.constructor.name).toEqual('Plot3DLoader')
   })
 
-  it("should have a xmlhttprequester for json files", function() {
+  it("should have a xmlhttprequester for gltf files", function() {
     expect(myPlot3DGlTfLoader.gltfRequester.constructor.name).toEqual("XMLHttpRequest")
+  })
+
+  it("should have an attribute which is a regular expression for extracting the raw data from a base64 buffer", function() {
+    expect(myPlot3DGlTfLoader.rawBufDataRegExMatcher).toEqual(/(?<=data:application\/octet-stream;base64,).*/)
   })
 
   it("should have a method for loading gltf files with embedded buffer data", function() {
@@ -21,12 +25,66 @@ describe("Plot3DGlTfLoader", function() {
     expect(typeof myPlot3DGlTfLoader.gltfRequester.onreadystatechange).toBe('function')
   })
 
+  describe("onreadystatechange", function() {
+    it("should log an error if the response status is not ok (!= 200)", function(done) {
+      spyOn(console, 'error')
+      myPlot3DGlTfLoader.requestGlTf('blablabla.gltf')
+      setTimeout(() => {
+        expect(console.error).toHaveBeenCalled()
+        done()
+      }, 300)
+    })
+
+    it("should call the class internal method for extracting the data from the gltf data", function(done) {
+      spyOn(myPlot3DGlTfLoader, "extractDataFromGltfJson")
+      let url = './spec/assets/mesh3d/cube.gltf'
+      myPlot3DGlTfLoader.requestGlTf(url)
+      setTimeout(() => {
+        expect(myPlot3DGlTfLoader.extractDataFromGltfJson).toHaveBeenCalled()
+        done()
+      }, 300)
+    })
+
+  })
+
+  it("should have a list attribute for storing the gltf data object", function() {
+    expect(myPlot3DGlTfLoader.loaded.constructor.name).toEqual('Array')
+  })
+
   describe("requestGlTf", function() {
     it("should call open with the url given in parameter 1", function() {
       spyOn(myPlot3DGlTfLoader.gltfRequester, 'open').and.callThrough()
+      spyOn(myPlot3DGlTfLoader.gltfRequester, 'send').and.callThrough()
       let url = './spec/assets/mesh3d/cube.gltf'
       myPlot3DGlTfLoader.requestGlTf(url)
       expect(myPlot3DGlTfLoader.gltfRequester.open).toHaveBeenCalledWith('GET', url, true)
+      expect(myPlot3DGlTfLoader.gltfRequester.send).toHaveBeenCalledWith(null)
+    })
+  })
+
+  it("should have a method, that extracts the mesh data from the gltf json", function() {
+    expect(typeof myPlot3DGlTfLoader.extractDataFromGltfJson).toBe('function')
+  })
+
+  describe("extractDataFromGltfJson", function() {
+    it("should log an error when the gltf object is not version 2", function(done) {
+      spyOn(console, 'error')
+      let url = './spec/assets/mesh3d/cubeWrongVersion.gltf'
+      myPlot3DGlTfLoader.requestGlTf(url)
+      setTimeout(() => {
+        expect(console.error).toHaveBeenCalledWith('check your gltf file, version must be 2.0')
+        done()
+      }, 300)
+    })
+
+    it("should extract the raw data from the base64 string", function(done) {
+      spyOn(window, 'atob').and.callThrough()
+      let url = './spec/assets/mesh3d/cube.gltf'
+      myPlot3DGlTfLoader.requestGlTf(url)
+      setTimeout(() => {
+        expect(atob).toHaveBeenCalled()
+        done()
+      }, 300)
     })
   })
 
--- public/dev/src/loaders/Plot3DGlTfLoader.js
+++ public/dev/src/loaders/Plot3DGlTfLoader.js
@@ -2,11 +2,15 @@ class Plot3DGlTfLoader extends Plot3DLoader{
   constructor() {
     super()
 
+    this.loaded = []
+
+    this.rawBufDataRegExMatcher = /(?<=data:application\/octet-stream;base64,).*/
+
     this.gltfRequester = new XMLHttpRequest()
     this.gltfRequester.onreadystatechange = (event) => {
       if (this.gltfRequester.readyState === 4) {
         if (this.gltfRequester.status === 200) {
-          console.log(this.gltfRequester.responseText)
+          this.extractDataFromGltfJson(JSON.parse(this.gltfRequester.responseText))
         } else {
            console.error(this.gltfRequester.statusText)
         }
@@ -17,6 +21,19 @@ class Plot3DGlTfLoader extends Plot3DLoader{
   requestGlTf(url) {
     this.gltfRequester.open('GET', url, true)
     this.gltfRequester.send(null)
-    console.log("send request")
+  }
+
+  extractDataFromGltfJson(gltfObject) {
+    if (gltfObject.asset.version !== '2.0') {
+      console.error('check your gltf file, version must be 2.0')
+    }
+    let data = this.rawBufDataRegExMatcher.exec(gltfObject.buffers[0].uri)[0]
+    let raw = window.atob(data)
+    // let len = raw.length
+    // let bytes = new Uint8Array(len)
+    // for (var i = 0; i < len; i++) {
+    //   bytes[i] = raw.charCodeAt(i)
+    // }
+    // console.log(bytes)
   }
 }
